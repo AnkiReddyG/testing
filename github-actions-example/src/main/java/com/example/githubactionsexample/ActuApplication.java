5d4876566edcb57347b56c099eb121ca2e8c3f63
VenkeyCool123 <venkey.g32@gmail.com>
2020-11-03 17:38:15 +0530

    new project add to testinggit status

---
+++ github-actions-example/src/main/java/com/example/githubactionsexample/ActuApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuApplication.class, args);
+	}
+
+}
---
+++ github-actions-example/src/test/java/com/example/githubactionsexample/ActuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
